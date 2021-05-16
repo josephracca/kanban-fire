b0dffd64687eeb0a503c5f073bd338d8f4157116
josephracca <josephracca@gmail.com>
2021-05-15 17:57:56 -0700

    working pre-firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './task/task';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private dialog: MatDialog){}
   title = 'kanban-fire';
+
+  todo: Task[] = [
+    {
+      title: 'Buy Milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!'
+    }
+  ];
+
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
+  
+  drop(event: CdkDragDrop<Task[]|null>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+  }
+
 }
